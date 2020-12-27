094bfc18c685621885ebb954d5daa0530eb66c44
Dinesh101041 <dinesh101041@gmail.com>
2020-12-27 21:46:08 +0530

    import threading

---
+++ main.py
@@ -0,0 +1,23 @@
+# threding allows to perform a multiple task and functions 
+import threading
+from threading import*
+
+import time
+
+# to store the key value and pair the data structure in python called dictionary is created
+dict={}
+
+def create(key,value,timeout=0):
+    # check the existig key
+    if key in dict:
+        print("keys is already exists")
+    else:
+        # checking dictionary length and key valuse is upto to memory limit is 1gb and 16 kb 
+        if.len(dict)<(1024*1020*1024) and value<=(16*1024*1024):
+            # time to live property
+            if timeout=0:
+                d=[value,timeout]
+            else:
+                d=[value,time.time()+timeout]
+                    
+            
