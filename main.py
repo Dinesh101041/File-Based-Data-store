c32e44d15f2c9bb755e2c8c90723a44188a7aa3e
Dinesh101041 <dinesh101041@gmail.com>
2020-12-27 21:47:42 +0530

    error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # threding allows to perform a multiple task and functions 
 import threading
-from threading import*
+# from threading import*
 
 import time
 
@@ -13,11 +13,11 @@ def create(key,value,timeout=0):
         print("keys is already exists")
     else:
         # checking dictionary length and key valuse is upto to memory limit is 1gb and 16 kb 
-        if.len(dict)<(1024*1020*1024) and value<=(16*1024*1024):
+        if len(dict)<(1024*1020*1024) and value<=(16*1024*1024):
             # time to live property
-            if timeout=0:
+            if timeout==0:
                 d=[value,timeout]
             else:
                 d=[value,time.time()+timeout]
-                    
+
             
