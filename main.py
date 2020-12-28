346ee8b02f99d6f159bd337fb07857c08c74d48a
Dinesh101041 <dinesh101041@gmail.com>
2020-12-28 12:20:11 +0530

    deletefunction

--- main.py
+++ main.py
@@ -19,5 +19,49 @@ def create(key,value,timeout=0):
                 d=[value,timeout]
             else:
                 d=[value,time.time()+timeout]
+            if len(key)<=32:
+                dict[key]=1
+        else:
+            print('Memory limit exceed should be less than 1 gb')
+
+
+
+# read operation
+
+def read(key):
+    # check the key in database
+    if key not in dict:
+        print("key is not present in database enter a valid key")
+    else:
+        a=dict[key]
+        # reading the time of the key 
+        if a[1]!==0:
+            if time.time()<a[1]:
+                # get the value in json format
+                string=str(key)+":"+str(a[0])
+                return string
+            else:
+                print("time is expired")
+        else:
+            string=str(key)+":"+str(a[0])
+            return string
+
+
+# delete operation in database
+
+def delete(key):
+    # check the key in database
+    if key not in dict:
+        print("key is not present in database enter a valid key")
+    else:
+        a=dict[key]
+        if a[1]!==0:
+            if time.time()<a[1]:
+                del dict[key]
+                
+        
+
+             
+
 
             
